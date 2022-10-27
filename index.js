9ce980b2505f5e62ba9c9c53ba67f5b80685dd6b
Dhinesh Kumar <97152497+dhineshrollins123@users.noreply.github.com>
2022-10-27 13:00:19 +0530

    Add files via upload
    JS added

---
+++ index.js
@@ -0,0 +1,67 @@
+var buttons = document.querySelectorAll("button");
+
+for (var i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener("click", function(){
+        keyCheck(this.textContent);
+        buttonAnimation(this.textContent);
+    });
+}
+
+document.addEventListener("keydown", function(event){
+    keyCheck(event.key);
+    buttonAnimation(event.key);
+});
+
+function keyCheck(buttonData) {
+
+    switch (buttonData) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        default:
+            alert("please check typed letter-"+buttonData+"-is not in arena !");
+            break;
+    }
+}
+
+function buttonAnimation(button){
+    var activeButton = document.querySelector("."+button);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100);
+}
